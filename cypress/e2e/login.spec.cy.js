6c817ff8a00086fee96e8a4b9897292e9ba5b4b1
Ya go <yago.souz.rodrigues@gmail.com>
2025-09-01 22:43:08 -0300

    Commit 3

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-title').contains('Dashboard' )
+    cy.wait(2000)
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('0')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
+   })
+})
